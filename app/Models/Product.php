47768ff440cc183e27270206872ae8875f469cb1
Mikyla Raquinio <micu.raquinio.up@phinmaed.com>
2025-02-19 20:08:19 +0800

    Update Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'price', 'image', 'category_id'];
+    protected $fillable = ['name', 'description', 'description', 'price', 'image', 'category_id'];
     protected $primaryKey = 'id'; // Optional, only if you have a custom primary key
 
 
